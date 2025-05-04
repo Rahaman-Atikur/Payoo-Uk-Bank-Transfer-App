036b326ddd2767f0052b1d74e50746b351cda5d6
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-05-04 23:34:08 +0100

    Redirect main html file desiging done

--- script/app.js
+++ script/app.js
@@ -1,4 +1,20 @@
 document.getElementById('login-btn')
-    .addEventListener('click', function () {
-        console.log('Hello');
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const accountNumber = document.getElementById('input-account-number').value;
+
+        const pinNumber = document.getElementById('input-pin-number').value;
+
+
+        const convertedPinNumber = parseInt(pinNumber);
+
+        if (accountNumber.length === 11) {
+            if (convertedPinNumber === 1234) {
+                window.location.href="./main.html"
+            }
+            else{
+                alert();
+            }
+        }
     })
